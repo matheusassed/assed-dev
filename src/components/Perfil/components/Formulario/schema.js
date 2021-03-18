57bdc2485c925c779245dcb0060c71580f6f84c6
Matheus Assed <matheus.assed@inpartec.com.br>
2021-03-18 01:53:12 -0300

    t

--- src/components/Perfil/components/Formulario/schema.js
+++ src/components/Perfil/components/Formulario/schema.js
@@ -4,5 +4,5 @@ export default Yup.object().shape({
   nome: Yup.string().min(2).required(),
   email: Yup.string().email().required(),
   mensagem: Yup.string().min(3).required(),
-  telefone: Yup.string().matches(/(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/g),
+  telefone: Yup.string().matches(/(\(?\d{2}\)?\s)?(\d{4,5}\\-\d{4})/g),
 });
